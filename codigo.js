2ac031558f1daf72cefd9d9b825905f15eb99196
MARTÍN GIL BLANCO <gilblancomartin@gmail.com>
2023-11-19 05:14:09 +0100

    index.html y codigo.js iniciales

--- codigo.js
+++ codigo.js
@@ -2,30 +2,31 @@ let edad = prompt("Por favor, introduce a túa idade:");
 edad = parseInt(edad);
 
 if (isNaN(edad)) {
-  alert(
-    "Por favor, introduce un número válido para a idade. Commit para arreglar Issue"
-  );
+  alert("Por favor, introduce un número válido para a idade.");
 } else {
   let categoria;
 
   switch (true) {
     case edad >= 0 && edad <= 12:
-      categoria = "ninhO";
+      categoria = "neno";
       break;
     case edad >= 13 && edad <= 18:
-      categoria = "muchachO";
+      categoria = "adolescente";
       break;
     case edad >= 19 && edad <= 30:
-      categoria = "xoveN";
+      categoria = "xoven";
       break;
-    case edad >= 31 && edad <= 100:
-      categoria = "adultO";
+    case edad >= 31 && edad <= 64:
+      categoria = "adulto";
+      break;
+    case edad >= 65 && edad <= 100:
+      categoria = "xubilado";
       break;
     default:
-      alert("Modificado en rebase.");
+      alert("Idade fora do rango permitido.");
   }
 
   if (categoria) {
-    alert("Eres un " + categoria + "editada en rama rebaseB.");
+    alert("Eres un " + categoria + ".");
   }
 }
